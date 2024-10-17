2bb18a3cc9fb34a1705b434573034077f0d32e42
Mileidy Vanegas Martínez <vanegasmartinezmileidy@gmail.com>
2024-10-17 13:42:35 -0500

    Jmm

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -1,1 +1,66 @@
-# TODO: Implementa el código del ejercicio aquí
+from abc import abstractmethod
+
+
+class ReglaValidación:
+    
+    def __init__(self, longitud_esperada: int):
+        self.longitud_esperada: int = longitud_esperada
+
+    def _validar_longitud(self, clave: str) -> bool:
+        if len(clave) > self.longitud_esperada:
+            return True
+        else:
+            return False
+
+    def _contiene_mayuscula(self, clave: str) -> bool:
+        
+        for i in clave:
+            if i.isupper():
+                return True
+        return False
+            
+    def _contiene_minuscula(self, clave: str) -> bool:
+        for i in clave:
+            if i.islower():
+                return True
+        return False
+
+    def _contiene_numero(self, clave: str) -> bool:
+        for i in clave:
+            if i.isdigit():
+                return True
+        return False
+
+    @abstractmethod
+    def es_valida(self, clave: str) -> bool:
+        pass
+
+
+class ReglaValidacionGanimedes:
+    
+    def __init__(self):
+        pass
+    
+    def contiene_caracter_especial(self, clave: str) -> bool:
+        for i in clave:
+            if i == "@" or i == "#" or i == "_" or i == "$" or i == "%":
+                return True
+        return False
+
+    def es_valida(self, clave: str) -> bool:
+        pass
+
+
+class ReglaValidacionCalisto:
+    
+    def __init__(self):
+        pass
+
+    def contiene_calisto(self, clave: str) -> bool:
+        pass
+
+    def es_valida(self, clave, str) -> bool:
+        pass
+
+class Validador:
+    pass
